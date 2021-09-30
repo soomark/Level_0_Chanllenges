5c35533effd554c85ec88f44b9c6b9cbc38fd583
soomark <91237579+soomark@users.noreply.github.com>
2021-09-30 10:12:13 +0200

    Add files via upload

---
+++ Task_0.1.py
@@ -0,0 +1,13 @@
+#Demonstrate the order of the program
+
+#Output of x and y equals 0 and 1
+x = 0
+y = 1
+print(x)
+print(y)
+
+#Output of x and y equals 3 and 4
+x += 3
+y += x
+print(x)
+print(y)
