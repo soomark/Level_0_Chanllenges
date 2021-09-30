b252a75521dad9efa21d45bbb323bab8c54af725
soomark <91237579+soomark@users.noreply.github.com>
2021-09-30 10:14:38 +0200

    Add files via upload

---
+++ Task_0.2.py
@@ -0,0 +1,26 @@
+#This demonstrates the BODMAS rule
+
+#Example x demonstrates multiplication 1st then addition resultant 3
+x = 1 + 1 * 2
+
+#Example y demonstrates addition within the brackets 1st then multiplication resultant 4
+y = (1 + 1) * 2
+
+#Example z demonstrates multiplication within the brackets 1st then addition resultant 3
+z = 1 + (1 * 2)
+
+#Example a demonstrates division 1st then multiplication 2nd and then addition resultant 2.0
+a = 1 + 1 * 2 / 2
+
+#Example b demonstrates multiplication 1st then addition within the brackets and then division resultant 1.5
+b = (1 + 1 * 2) / 2
+
+#Uncetain as to comment ''print all the thing'' in question.
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
+
+#Or
+print('print all the thing')
