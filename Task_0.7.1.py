a5c2b6131e4fbc4b8f042df8bac5abe41736d304
soomark <91237579+soomark@users.noreply.github.com>
2021-10-03 17:41:56 +0200

    Delete Task_0.7.1.py

--- Task_0.7.1.py
+++
@@ -1,9 +0,0 @@
-# Function to convert  Fahrenheit to Celsius
-def celc(c):
-    celsius = (c - 32) * 5/9  # Formula to convert Celsius to Fahrenheit
-    print('%.2f degree Fahrenheit is equal to %0.2f degree Celsius' % (c, celsius))
-
-
-c = input('Input the Fahrenheit value: ')
-c = float(c)
-celc(c)
