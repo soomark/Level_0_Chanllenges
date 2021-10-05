c3c589269829ca7db2de0568e8b598411b4984c1
soomark <91237579+soomark@users.noreply.github.com>
2021-10-05 08:15:49 +0200

    Add files via upload

--- Task_0.8.py
+++ Task_0.8.py
@@ -1,15 +1,9 @@
-# Function converts seconds to hour and minutes
-def time_conversion(seconds):
-    # Convert seconds to hour
-    hours = seconds // 3600
-
-    # Converts seconds to minute
-    seconds %= 3600
-    minutes = seconds // 60
-    seconds %= 60
-
-    print("%0d hours %0d minutes " % (minutes, seconds))
+# Function converts any number to hours and minutes
+def any_number(num):
+    h = num // 60
+    m = num % 60
+    print(h, 'hours', m, 'minutes')
 
 
-seconds_input = (int(input("Enter the Time in seconds: ")))
-time_conversion(seconds_input)
+n = 133
+any_number(n)
