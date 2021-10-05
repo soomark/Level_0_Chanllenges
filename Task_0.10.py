e6792deb5aa458f7bd536fab13a857b536a1a61a
soomark <91237579+soomark@users.noreply.github.com>
2021-10-05 08:16:05 +0200

    Add files via upload

--- Task_0.10.py
+++ Task_0.10.py
@@ -10,15 +10,15 @@ def common_letters(word1, word2):
 	if len(s1) < len(s2):
 		for i in s1:
 			if i in s2:
-				result.append(i)
+				result.append(i + ', ')
 	else:
 		for i in s2:
 			if i in s1:
-				result.append(i)
+				result.append(i + ', ')
 
 	print("Common letters:", *result)
 
 
-wordA = input("Enter the first string: ")
-wordB = input("Enter the second string: ")
+wordA = "house"
+wordB = "computers"
 common_letters(wordA, wordB)
