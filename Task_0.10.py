14ae4116274aff5db8a8cb20257e937acb3ac95c
soomark <91237579+soomark@users.noreply.github.com>
2021-10-07 10:26:52 +0200

    Add files via upload

--- Task_0.10.py
+++ Task_0.10.py
@@ -1,24 +1,22 @@
-#Function to find common elements present in 2 words
+# Function to find common elements present in 2 words
 def common_letters(word1, word2):
 	s1 = word1
 	s2 = word2
-
-#List output
-	result = []
-
-#Function process
+	# List output
+	result = ''
+	# Function process
 	if len(s1) < len(s2):
 		for i in s1:
-			if i in s2:
-				result.append(i + ', ')
+			if i.lower() in s2.lower():
+				result += i + ', '
 	else:
 		for i in s2:
-			if i in s1:
-				result.append(i + ', ')
-
-	print("Common letters:", *result)
+			if i.lower() in s1.lower():
+				result += i + ', '
+	return result
 
 
 wordA = "house"
 wordB = "computers"
-common_letters(wordA, wordB)
+result = common_letters(wordA, wordB)
+print(" 'Common letters:", result[:len(result) - 2] + '\'')
