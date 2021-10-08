bca3d3d1abe3bea1e459b96f9c1be70fc7370b69
soomark <91237579+soomark@users.noreply.github.com>
2021-10-08 11:32:39 +0200

    Update Task_0.9.py

--- Task_0.9.py
+++ Task_0.9.py
@@ -5,8 +5,11 @@ def printvowels(word):
     for x in word:
         if x in 'aeiou':
             letters += x + ','
-    print(letters[:len(letters) - 1])
+        else:
+            if x in 'AEOIU':
+                            letters += x + ','
+    print(letters.lower()[:len(letters) - 1])
 
 
-w1 = "Umuzi"
-printvowels(w1)
+input_string = "Ear"
+printvowels(input_string)
