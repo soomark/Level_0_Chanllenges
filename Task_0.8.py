1f219adfd21174c254eb6521226966dc967b98fe
soomark <91237579+soomark@users.noreply.github.com>
2021-10-11 12:30:06 +0200

    Update Task_0.8.py

--- Task_0.8.py
+++ Task_0.8.py
@@ -1,10 +1,12 @@
-# Function converts any number to hour and minutes
+
 def any_number(num):
     h = num // 60
     m = num % 60
+    if h > 1 and m > 1:
+        print(h, 'hours,', m, 'minutes')
+    else:
+        print(h, 'hour,', m, 'minute')
 
-    print(h, 'hour,', m, 'minute')
 
-
-n = 61
+n = 133
 any_number(n)
